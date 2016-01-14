aa60b783ef9e13c896b3145885982f14c9658f36
Andrea Dardón <Andardon1111@gmail.com>
2016-01-14 19:29:53 +0100

    trying serving fallback

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Place.create(name: "Split, Croatia", description: "A nice studio apartment in the heart of Split. It's beautifully decorated and it is a nice place to come and rest after a long day working or touristing. The location is perfect and there are supermarkets and restaurants a couple of steps away. It is also located on the road to a beautiful park that have some of the nicest views of Split. The only bad thing is that since it is located on the ground floor of a highly transited street, the noises can be very loud making it hard to sleep or waking you up early in the mornings.", created_at: "2016-01-04 11:23:32", updated_at: "2016-01-14 16:54:28", image_url: "portablu1.png", internet_speed: "Over 30mbps", short_term_rental: "Yes", level_of_noise: "Bad", walkability: "Great", food: "Accessible", ac_heater: "Only AC")
